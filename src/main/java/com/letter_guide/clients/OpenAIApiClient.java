98001f522970c36c77c4efd5b2d43fbf50e0ab06
Furkan Asani <furkan@chrono24.com>
2023-06-23 15:40:57 +0200

    Refactored the OpenAIApiClient and implemented a new test which checks the createRequest logic

--- src/main/java/com/letter_guide/clients/OpenAIApiClient.java
+++ src/main/java/com/letter_guide/clients/OpenAIApiClient.java
@@ -7,6 +7,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,37 @@ import org.springframework.stereotype.Component;
 @PropertySource("classpath:secrets.properties")
 @Component
 public class OpenAIApiClient {
+   @Autowired
    private final HttpClient httpClient;
    private static final String OPEN_AI_API = "https://api.openai.com/v1/chat/completions";
    @Value("${openai.api.key}")
    private  String API_KEY;
 
+   @Autowired
    public OpenAIApiClient( HttpClient httpClient ) {
       this.httpClient = httpClient;
+
    }
 
    public String sendPrompt( String body ) {
-      HttpRequest request = null;
+      HttpRequest request = createRequest(body);
+
+      HttpResponse<String> response;
+      try {
+         response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+         if ( response.statusCode() >= 400 ) {
+            throw new RuntimeException("The OpenAI api must be unavailable due to a reason. Response data: "+ response.body());
+         }
+
+         return response.body();
+      }
+      catch ( IOException | InterruptedException e ) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   protected HttpRequest createRequest( String body ) {
+      HttpRequest request;
       try {
          request = HttpRequest.newBuilder()
                .uri(new URI(OPEN_AI_API))
@@ -37,19 +58,7 @@ public class OpenAIApiClient {
       catch ( URISyntaxException e ) {
          throw new RuntimeException(e);
       }
-
-      HttpResponse<String> response = null;
-      try {
-         response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      }
-      catch ( IOException | InterruptedException e ) {
-         throw new RuntimeException(e);
-      }
-      if ( response.statusCode() >= 400 ) {
-         throw new RuntimeException("The OpenAI api must be unavailable due to a reason. Response data: "+ response.body());
-      }
-
-      return response.body();
+      return request;
    }
 
 }
--- src/test/java/com/letter_guide/clients/OpenAIApiClientTest.java
+++ src/test/java/com/letter_guide/clients/OpenAIApiClientTest.java
@@ -7,6 +7,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,19 +24,42 @@ class OpenAIApiClientTest {
    @InjectMocks
    private OpenAIApiClient client;
 
+   @BeforeEach
+   void setUp() {
+
+      MockitoAnnotations.openMocks(this);
+   }
+
    @SneakyThrows
    @Test
    void testSendPrompt() {
-      MockitoAnnotations.openMocks(this);
-
+      //given
       HttpResponse<String> httpResponse = Mockito.mock(HttpResponse.class);
       String responseBody = "{ \"id\": \"chatcmpl-6p9XYPYSTTRi0xEviKjjilqrWU2Ve\", [...] }";
       Mockito.when(httpResponse.body()).thenReturn(responseBody);
       Mockito.when(httpResponse.statusCode()).thenReturn(200);
-
       Mockito.when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class))).thenReturn(httpResponse);
 
+      //when
       String response = client.sendPrompt("aaa");
+
+      //then
       assertThat(response.equals(responseBody));
+   }
+
+   @Test
+   void testCreateRequest() {
+      //given
+      String promptRequestBody = "aaa";
+
+      //when
+      HttpRequest request = client.createRequest(promptRequestBody);
+
+      //then
+      assertThat(request).isNotNull();
+      assertThat(request.uri()).hasHost("api.openai.com");
+      assertThat(request.method()).isEqualTo("POST");
+      assertThat(request.headers()).isNotNull();
+      assertThat(request.headers().map()).containsKey("Authorization");
    }
 }
