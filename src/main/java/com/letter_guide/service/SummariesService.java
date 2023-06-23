cd7f0a7e1c5746aef12dfad0f6f266dfcd289459
Furkan Asani <furkan@chrono24.com>
2023-06-23 10:30:23 +0200

    Create a summaries service

---
+++ src/main/java/com/letter_guide/service/SummariesService.java
@@ -0,0 +1,26 @@
+package com.letter_guide.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.letter_guide.models.SummarizedDocument;
+import com.letter_guide.repository.SummariesRepository;
+
+
+@Service
+public class SummariesService{
+
+   private final SummariesRepository _summariesRepository;
+
+   @Autowired
+   public SummariesService(SummariesRepository summariesRepository) {
+      this._summariesRepository= summariesRepository;
+   }
+
+   public List<SummarizedDocument> getAllDocumentsByUserId(String userId) {
+      return _summariesRepository.findByUserId(userId);
+
+   }
+}
