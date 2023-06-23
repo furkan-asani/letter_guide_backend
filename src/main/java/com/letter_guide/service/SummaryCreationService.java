8e78deedd9a68b42f9da8063f3c45f0014967cb6
Furkan Asani <furkan@chrono24.com>
2023-06-23 15:14:41 +0200

    Implement a SummaryCreationService which is responsible for making the appropriate API calls in order to summarize the letter

---
+++ src/main/java/com/letter_guide/service/SummaryCreationService.java
@@ -0,0 +1,52 @@
+package com.letter_guide.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.letter_guide.clients.OpenAIApiClient;
+import com.letter_guide.models.api.OpenAIApiRequest;
+
+
+@Service
+public class SummaryCreationService {
+
+   public static final String SYSTEM = "system";
+   public static final String GPT_3_5_TURBO = "gpt-3.5-turbo";
+   @Value("${openai.api.key}")
+   private String apiKey; // Your OpenAI API key
+
+   @Autowired
+   private  OpenAIApiClient _openAIApiClient;
+   @Autowired
+   private PromptCreationService _promptCreationService;
+
+   public String sendPrompt(String extractedText) {
+
+      String apiRequestBody = createOpenAIApiRequest(extractedText);
+      String responseBody = _openAIApiClient.sendPrompt(apiRequestBody);
+
+      return null;
+   }
+
+   protected String createOpenAIApiRequest( String extractedText ) {
+      String prompt = _promptCreationService.createPrompt(extractedText);
+
+      ArrayList<OpenAIApiRequest.Message> messages = new ArrayList<>();
+      messages.add(new OpenAIApiRequest.Message(SYSTEM, prompt));
+
+
+      OpenAIApiRequest openAIApiRequest = new OpenAIApiRequest(GPT_3_5_TURBO, messages, 0.2);
+      ObjectMapper objectMapper = new ObjectMapper();
+      try {
+         return objectMapper.writeValueAsString(openAIApiRequest);
+      }
+      catch ( JsonProcessingException e ) {
+         throw new RuntimeException("Error while writing the request object into a json body",e);
+      }
+   }
+}
