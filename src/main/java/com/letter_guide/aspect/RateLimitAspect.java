b81461b2f3d460df11b9757ec47c86d3ef4a6bbb
Furkan Asani <furkan@chrono24.com>
2023-06-23 16:59:33 +0200

    Create the RateLimit Annotation and Aspect

---
+++ src/main/java/com/letter_guide/aspect/RateLimitAspect.java
@@ -0,0 +1,39 @@
+package com.letter_guide.aspect;
+
+import java.util.concurrent.TimeUnit;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Aspect
+@Component
+public class RateLimitAspect {
+
+   @Autowired
+   private HttpServletRequest request;
+
+   @Autowired
+   private RedisTemplate<String, String> redisTemplate;
+
+   @Before("@annotation(com.letter_guide.aspect.RateLimit)")
+   public void rateLimit() {
+      String ip = request.getRemoteAddr();
+      ValueOperations<String, String> ops = redisTemplate.opsForValue();
+
+      long count = ops.increment(ip, 1);
+      if (count == 1) {
+         redisTemplate.expire(ip, 1, TimeUnit.DAYS);
+      }
+
+      if (count > 3) {
+         throw new RuntimeException("Too many requests");
+      }
+   }
+}
