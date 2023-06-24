59a4aeb095729b0bbcbefa647aa7b2329cb14583
Furkan Asani <furkan@chrono24.com>
2023-06-24 14:35:06 +0200

    Adjust the ParserService to first parse it into the response model and extract the content from that responseModel before trying to parse it into a summarizedDocument

--- src/main/java/com/letter_guide/service/OpenAIResponseParserService.java
+++ src/main/java/com/letter_guide/service/OpenAIResponseParserService.java
@@ -3,8 +3,11 @@ package com.letter_guide.service;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.letter_guide.models.SummarizedDocument;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.letter_guide.models.api.openAI.response.Choice;
+import com.letter_guide.models.api.openAI.response.OpenAIChatCompletionResponse;
 
 
 @Service
@@ -12,11 +15,28 @@ public class OpenAIResponseParserService {
    public SummarizedDocument parseResponse(String openAIResponse) {
       ObjectMapper mapper = new ObjectMapper();
       try {
+         OpenAIChatCompletionResponse openAIChatCompletionResponse = parseOpenAIResponseIntoResponseModel(openAIResponse, mapper);
 
-         return mapper.readValue(openAIResponse, SummarizedDocument.class);
+         return parseResponseModelIntoSummarizedDocument(mapper, openAIChatCompletionResponse);
       }
       catch ( JsonProcessingException e ) {
          throw new RuntimeException("The response could not be parsed correctly. OpenAI responded with the following: openAIResponse", e);
       }
+   }
+
+   private SummarizedDocument parseResponseModelIntoSummarizedDocument( ObjectMapper mapper, OpenAIChatCompletionResponse openAIChatCompletionResponse )
+         throws JsonProcessingException {
+      Choice choice = openAIChatCompletionResponse.getChoices().get(0);
+      if ( choice == null ) {
+         throw new RuntimeException("The response is invalid since it does not contain the necessary data. Response: " + openAIChatCompletionResponse);
+      }
+
+      return mapper.readValue(choice.getMessage().getContent(), SummarizedDocument.class);
+   }
+
+   private OpenAIChatCompletionResponse parseOpenAIResponseIntoResponseModel( String openAIResponse, ObjectMapper mapper ) throws JsonProcessingException {
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+      return mapper.readValue(openAIResponse, OpenAIChatCompletionResponse.class);
    }
 }
