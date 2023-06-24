f24449c30a97e61b311827d5949f74cb67428398
Furkan Asani <furkan@chrono24.com>
2023-06-24 12:57:26 +0200

    Fix warnings Use textblock and remove unnecessary import

--- src/test/java/com/letter_guide/service/PromptCreationServiceTest.java
+++ src/test/java/com/letter_guide/service/PromptCreationServiceTest.java
@@ -1,7 +1,6 @@
 package com.letter_guide.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,9 +19,19 @@ class PromptCreationServiceTest {
       String prompt = _promptCreationService.createPrompt(extractedText);
 
       //then
-      assertThat(prompt).isEqualTo("I want you to act as an expert translator and summarizer. I will provide you with extracted text from a german letter containing hard to understand 'government talk' and you will categorize it, summarize it, extract possible tasks or next steps if there are any, extract possible deadlines if there are any and you will decide on the priority and importance of this letter. You will provide this response in a json format which will look like this:\n"
-            + "{\n" + "  category,\n" + "  summary,\n" + "  tasks,\n" + "  nextSteps,\n" + "  deadlines,\n" + "  priority\n" + "}\n"
-            + "The priority can be either low, medium or high. Please provide the date in the following format: 'dd-MM-YYY'. Tasks and next steps should be arrays with the respective tasks in the correct order. Summary and category can be simple strings.\n"
-            + "The text you should process: \n" + "\n" + "This is a sample text which should be analyzed and summarized");
+      assertThat(prompt).isEqualTo("""
+            I want you to act as an expert translator and summarizer. I will provide you with extracted text from a german letter containing hard to understand 'government talk' and you will categorize it, summarize it, extract possible tasks or next steps if there are any, extract possible deadlines if there are any and you will decide on the priority and importance of this letter. You will provide this response in a json format which will look like this:
+            {
+              category,
+              summary,
+              tasks,
+              nextSteps,
+              deadlines,
+              priority
+            }
+            The priority can be either low, medium or high. Please provide the date in the following format: 'dd-MM-YYY'. Tasks and next steps should be arrays with the respective tasks in the correct order. Summary and category can be simple strings.
+            The text you should process:\s
+
+            This is a sample text which should be analyzed and summarized""");
    }
 }
