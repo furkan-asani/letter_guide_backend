9d4186ff8f0a211e53f94b16403fd52cd08867a6
Furkan Asani <furkan@chrono24.com>
2023-07-01 10:56:06 +0200

    Update OpenAIChatCompletionResponse and Usage class for MongoDB storage
    The OpenAIChatCompletionResponse class and its inner class Usage are updated to boost functionality in MongoDB. This class now serves as a MongoDB document, indicated by the Document annotation. The ID field is now an actual ID recognized by MongoDB, stemming from the Id annotation. Functionality for data access in the Usage class is also expanded to keep track of prompt tokens, completion tokens, and total tokens. These changes aim to improve data organization, management, and increase in database efficiency.

--- src/main/java/com/letter_guide/models/api/openAI/response/OpenAIChatCompletionResponse.java
+++ src/main/java/com/letter_guide/models/api/openAI/response/OpenAIChatCompletionResponse.java
@@ -2,62 +2,68 @@ package com.letter_guide.models.api.openAI.response;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Document(collection = "parsedResponses")
 public class OpenAIChatCompletionResponse {
+
+   @Id
    private String id;
-
-   @Override
-   public String toString() {
-      return "OpenAIChatCompletionResponse{" + "id='" + id + '\'' + ", object='" + object + '\'' + ", created=" + created + ", choices=" + choices + ", usage="
-            + usage + '}';
-   }
-
    private String       object;
    private long         created;
    private List<Choice> choices;
    private Usage        usage;
 
-   public String getId() {
-      return id;
-   }
-
-   public void setId( String id ) {
-      this.id = id;
-   }
-
-   public String getObject() {
-      return object;
-   }
-
-   public void setObject( String object ) {
-      this.object = object;
+   public List<Choice> getChoices() {
+      return choices;
    }
 
    public long getCreated() {
       return created;
    }
 
-   public void setCreated( long created ) {
-      this.created = created;
+   public String getId() {
+      return id;
    }
 
-   public List<Choice> getChoices() {
-      return choices;
-   }
-
-   public void setChoices( List<Choice> choices ) {
-      this.choices = choices;
+   public String getObject() {
+      return object;
    }
 
    public Usage getUsage() {
       return usage;
    }
 
+   public void setChoices( List<Choice> choices ) {
+      this.choices = choices;
+   }
+
+   public void setCreated( long created ) {
+      this.created = created;
+   }
+
+   public void setId( String id ) {
+      this.id = id;
+   }
+
+   public void setObject( String object ) {
+      this.object = object;
+   }
+
    public void setUsage( Usage usage ) {
       this.usage = usage;
+   }
+
+   @Override
+   public String toString() {
+      return "OpenAIChatCompletionResponse{" + "id='" + id + '\'' + ", object='" + object + '\'' + ", created=" + created + ", choices=" + choices + ", usage="
+            + usage + '}';
    }
 
    // Getters and setters
@@ -66,38 +72,37 @@ public class OpenAIChatCompletionResponse {
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Usage {
+
    @JsonProperty("prompt_tokens")
    private int promptTokens;
-
-   public int getPromptTokens() {
-      return promptTokens;
-   }
-
-   public void setPromptTokens( int promptTokens ) {
-      this.promptTokens = promptTokens;
-   }
+   @JsonProperty("completion_tokens")
+   private int completionTokens;
+   @JsonProperty("total_tokens")
+   private int totalTokens;
 
    public int getCompletionTokens() {
       return completionTokens;
    }
 
-   public void setCompletionTokens( int completionTokens ) {
-      this.completionTokens = completionTokens;
+   public int getPromptTokens() {
+      return promptTokens;
    }
 
    public int getTotalTokens() {
       return totalTokens;
    }
 
+   public void setCompletionTokens( int completionTokens ) {
+      this.completionTokens = completionTokens;
+   }
+
+   public void setPromptTokens( int promptTokens ) {
+      this.promptTokens = promptTokens;
+   }
+
    public void setTotalTokens( int totalTokens ) {
       this.totalTokens = totalTokens;
    }
-
-   @JsonProperty("completion_tokens")
-   private int completionTokens;
-
-   @JsonProperty("total_tokens")
-   private int totalTokens;
 
    // Getters and setters
 }
